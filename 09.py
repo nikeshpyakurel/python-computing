f3b94c1ebefad1f05601297ce3f7db6c2f6b6c91
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-07-02 12:57:30 +0545

    all program

--- 09.py
+++ 09.py
@@ -1,11 +1,7 @@
 givenString = input("Enter The String:")
 reversedString = ''
 counter = 0
-lengthOfString = givenString.__len__()-1
-decreace = lengthOfString
-while counter <= lengthOfString:
-    reversedString = reversedString[counter]+givenString[decreace]
+while counter < len(givenString):
+    reversedString = givenString[counter]+reversedString
     counter = counter+1
-    decreace = decreace-1
-
 print(reversedString)
---
+++ 10.py
@@ -0,0 +1,6 @@
+givenString = input("Enter The String:")
+reversedString = ''
+counter = 0
+for i in givenString:
+    reversedString = i+reversedString
+print(reversedString)
